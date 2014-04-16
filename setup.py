825f2e20578bb4f4e2445763be8af974aa1f1f53
robnagler <github>
2014-04-16 15:43:21 -0600

    Make builds easier

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+setup(
+    name='WPG',
+    version='0.0.1',
+    description='WaveProperGator is an interactive simulation framework for coherent X-ray wavefront propagation.',
+    author='A. Buzmakov',
+    packages=find_packages()
+    );
+
